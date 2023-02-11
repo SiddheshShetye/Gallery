34e5b8c99b0e3c21b8375c9a180ac3d67e3287ee
Siddhesh Shetye <siddhesh.shetye@leadschool.in>
2023-02-12 02:23:52 +0530

    set up main activity as container to add fragments. added new photo grid fragment

--- app/src/main/java/com/siddroid/gallery/MainActivity.kt
+++ app/src/main/java/com/siddroid/gallery/MainActivity.kt
@@ -2,10 +2,25 @@ package com.siddroid.gallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.siddroid.gallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        binding.toolbar.title = getString(R.string.app_name)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<PhotoGridFragment>(R.id.container)
+            }
+        }
     }
 }
